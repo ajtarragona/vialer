56d1e374975ddfb0c20accbf5633db4ffb772370
Txomin Medrano Martorell <tmedrano@tarragona.cat>
2020-05-08 12:59:16 +0200

    lang

--- src/resources/lang/ca/vialer.php
+++ src/resources/lang/ca/vialer.php
@@ -36,9 +36,10 @@ return [
     "Longitud" => "Longitud",
     "Afegir marcador" => "Afegir marcador",
     "Sembla que l'adreça que busques no existeix." => "Sembla que l'adreça que busques no existeix.",
-    "Potser et refereixes a alguna d'aquestes" => "Potser et refereixes a alguna d'aquestes",
+    "Potser et refereixes a alguna d'aquestes?" => "Potser et refereixes a alguna d'aquestes?",
     "Recomanacions d'ús" => "Recomanacions d'ús",
     "not_found_helptext" => "<p>Només pots buscar carrers del municipi de Tarragona.</p>
+        <p>No introdueixis el tipus de carrer a la cerca: les paraules carrer, rambla, plaça, etc.</p>
         <p>Només es busca a partir dels camps carrer, numero, escala, pis i porta. La resta els pots emplenar però no es faran servir per buscar.</p>
         <p>Mira de no afinar tant la búqueda. Per exemple, busca només per número i ja triaras el pis i porta d'entre els resultats mostrats.</p>
         <p>Quan triis un domicili, s'emplenaran la resta de camps automàticament.</p>"
--- src/resources/lang/es/vialer.php
+++ src/resources/lang/es/vialer.php
@@ -36,9 +36,10 @@ return [
     "Longitud" => "Longitud",
     "Afegir marcador" => "Añadir marcador",
     "Sembla que l'adreça que busques no existeix." => "Parece que la dirección que buscas no existe.",
-    "Potser et refereixes a alguna d'aquestes" => "Puede que te refieras a alguna de estas",
+    "Potser et refereixes a alguna d'aquestes?" => "¿Puede que te refieras a alguna de estas?",
     "Recomanacions d'ús" => "Recomendaciones de uso",
     "not_found_helptext" => "<p>Solo puedes buscar domicilios del municipio de Tarragona.</p>
+        <p>No introduzcas el tipo de calle a la búsqueda: las palabras calle, plaza, rambla, etc.</p>
         <p>Solo se busca a partir de los campos calle, número, escalera, planta y puerta. El resto los puedes rellenar pero no se usarán en la búsqueda.</p>
         <p>Intenta no afinar demasiado la búsqueda. Por ejemplo, busca solo por número y ya elegirás la planta y puerta de entre las opciones mostradas.</p>
         <p>Cuando elijas un domicilio, se rellenarán el resto de campos automáticamente.</p>"
